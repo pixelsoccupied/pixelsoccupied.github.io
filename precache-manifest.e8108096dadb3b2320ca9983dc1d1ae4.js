5ecbab6d64c79e42aa9b4157a6d7f550697a6033
np <nahian@consiliumcrypto.ai>
2019-12-12 02:19:00 -0500

    Updates

---
+++ precache-manifest.e8108096dadb3b2320ca9983dc1d1ae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09a8e919a4a281376bb306010b285d4",
+    "url": "/w/index.html"
+  },
+  {
+    "revision": "daa2bfb97ed163af625d",
+    "url": "/w/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "84500dc0559fff7505a1",
+    "url": "/w/static/js/2.51e59382.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/w/static/js/2.51e59382.chunk.js.LICENSE"
+  },
+  {
+    "revision": "daa2bfb97ed163af625d",
+    "url": "/w/static/js/main.28b30aae.chunk.js"
+  },
+  {
+    "revision": "758a32e65596c6d51a6f",
+    "url": "/w/static/js/runtime-main.113f8d04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/w/static/media/logo.5d5d9eef.svg"
+  }
+]);
